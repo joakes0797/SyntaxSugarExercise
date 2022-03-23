b682b402d4d9599a435bd8cec2b8c3fcb5a4556e
Jill Oakes <jericho_rules@hotmail.com>
2022-03-23 14:46:24 -0400

    Initialize Syntax Sugar exercise

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SyntaxSugar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Given the following code, change the code to implement:
+            //inferred typing, string interpolation,and the ternary operator.
+            //int answer = 4;
+            //string response;
+            //if (answer = 9)
+            //{
+            //    response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            //    response = answer + “greater than or equal to nine”;
+            //}
+
+            //var answer = 4;
+            ////var response;
+            //if (answer < 9)
+            //{
+            //    Console.WriteLine($"{answer} is less than nine");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"{answer} is greater than or equal to nine");
+            //}
+
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+            Console.WriteLine(response);
+
+        }
+    }
+}
